bfb7cdd37b2c7dd2c7d016dbef24892b10184d92
Andi Fahrul Azis <fachrulmosaja@gmail.com>
2025-11-22 17:41:47 +0800

    config vercel

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -2,6 +2,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const PORT = process.env.PORT || "";
+export const PORT = process.env.PORT || "http://localhost:3000";
 export const DATABASE_URL = process.env.DATABASE_URL || "";
 export const JWT_SECRET = process.env.JWT_SECRET || "";
